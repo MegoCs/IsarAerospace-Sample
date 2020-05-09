30cb5d1f848d48ce5e3d847344fa55640ce8ed90
Ahmed Kamel <Ahmed.Kamel@linkdev.com>
2020-05-09 03:14:53 +0200

    Add project files.

---
+++ IsarAerospace.CsvLoader/Record.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IsarAerospace.CsvLoader
+{
+    public class Record
+    {
+        public string Title { get; set; }
+        public string Author {get;set;}
+        public int Year {get;set;}
+        public double Price {get;set;}
+        public bool InStock {get;set;}
+        public List<string> Binding {get;set;}
+        public string FirstBinding { get {
+                return Binding?.FirstOrDefault()??string.Empty;
+            }
+        }
+        public string Description{get;set;}        
+    }
+}
---
+++ IsarAerospace.DataViewer/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using IsarAerospace.CsvLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace IsarAerospace.DataViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitSampleData();
+        }
+
+        private void InitSampleData()
+        {
+            Random random = new Random();
+            List<Record> data = new List<Record>();
+            for (int i = 0; i < 50; i++)
+            {
+                data.Add(new Record()
+                {
+                    Title = $"Title {i}",
+                    Author = $"Author {i} ",
+                    Year = random.Next(1990,2020),
+                    Price = random.NextDouble()*10000,
+                    InStock = random.Next()%2==0,
+                    Binding = new List<string>(){
+                    "Winner of the 1973 National Book Award",
+                    "Gravity's Rainbow is a postmodern epic",
+                    "a work as exhaustively significant to the second half of the 20th century as Joyce's Ulysses was to the first. Its sprawling",
+                    "encyclopedic narrative, and penetrating analysis of the impact of technology on society make it an intellectual tour de force. Ignition!: An informal history of liquid rocket propellants"
+                    },
+                    Description = $"This is Awsome {i}"
+                });
+            }
+            data=data.OrderByDescending(x => x.Price).ToList();
+            mainDataGrid.ItemsSource = data;
+        }
+    }
+}
